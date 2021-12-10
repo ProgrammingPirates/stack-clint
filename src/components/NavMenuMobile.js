1e5bc589785da9c75bdb9b171571e7e89aa4f5ca
Dharmendra solanki <78801686+ProgrammingPirates@users.noreply.github.com>
2021-12-10 23:35:57 +0530

    Update NavMenuMobile.js

--- src/components/NavMenuMobile.js
+++ src/components/NavMenuMobile.js
@@ -86,12 +86,12 @@ const MobileNavMenu = () => {
             Made with{' '}
             <FavoriteIcon style={{ fontSize: 10, color: '#f4649f' }} /> by{' '}
             <Link
-              href={'https://github.com/amand33p'}
+              href={''}
               color="inherit"
               target="_blank"
               rel="noopener"
             >
-              <strong>{` amand33p`}</strong>
+              <strong>{`Dharmendra`}</strong>
             </Link>
           </Typography>
         </div>
